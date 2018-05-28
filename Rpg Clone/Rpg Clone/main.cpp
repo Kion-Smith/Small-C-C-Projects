fc31725b24802f2eef02241d5ea797eb54f29e0e
Kion Smith <30888048+Kion-Smith@users.noreply.github.com>
2018-05-27 20:13:52 -0500

    created play area and character

--- Rpg Clone/Rpg Clone/main.cpp
+++ Rpg Clone/Rpg Clone/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <windows.h>
-int playFieldWidth = 20;
+
+bool checkNextMove(int, int, int);
+
+int playFieldWidth = 50;
 int playFieldHeight = 20;
+
 
 int consoleWidth = 80;
 int consoleHeight =50;
 
+
 unsigned char *playField = nullptr;
+
 
 int main()
 {
@@ -17,7 +23,7 @@ int main()
 	{
 		for (int y = 0; y < playFieldHeight; y++)
 		{
-			//playField[y*playFieldWidth + x] =
+			playField[y*playFieldWidth + x] = ( x==0 || y==0 ||x == playFieldWidth - 1 || y == playFieldHeight - 1) ? 2 : 1;
 		}
 	}
 
@@ -33,17 +39,43 @@ int main()
 	SetConsoleActiveScreenBuffer(console);
 	DWORD dwBytesWritten = 0;
 
+	//game loop items
 	bool isRunning = true;
+	bool keys[4];
+
+	// player items
+	int playerChar = 3;
+	int curX = playFieldWidth/ 2;
+	int curY = playFieldHeight/2;
 
 	while (isRunning)
 	{
+
+		for (int k = 0; k < 4; k++)
+		{
+																// left,up,right,down		
+			keys[4] = (0x8000 & GetAsyncKeyState((unsigned char)("\x25\x26\x27\x28"[k]))) != 0;
+		}
+
 		for (int x = 0; x < playFieldWidth; x++)
 		{
 			for (int y = 0; y < playFieldHeight; y++)
 			{
-				//screen[(y)*playFieldWidth + (x)] = L" ABCDEFG=#"[playField[y*playFieldWidth + x]];
+				screen[(y+2)*consoleWidth + (x+4)] = L" \u2591\u2588"[playField[y*playFieldWidth + x]];
 			}
 		}
+
+		//this will be player movement 
+		if (false) //add some way to check player movements
+		{
+
+		}
+		else
+		{
+
+		}
+		//drawing player
+		screen[curY*consoleWidth + curX] = L'\u263B';
 
 		WriteConsoleOutputCharacter(console, screen, consoleWidth*consoleHeight, { 0,0 } ,&dwBytesWritten);
 	}
@@ -53,3 +85,4 @@ int main()
 
 	return 0;
 }
+
